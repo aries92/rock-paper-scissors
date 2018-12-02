f10a2153089bf6283fa38371c998abb2e00cbe99
aries92 <ded.lviv@gmail.com>
2018-12-02 15:13:16 +0200

    Updates

---
+++ precache-manifest.7a947f56cc7eb90c38c99e0b00ac9822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f2f0cdf0c221f8d98a",
+    "url": "/rock-paper-scissors/static/js/runtime~main.62f2f0cd.js"
+  },
+  {
+    "revision": "f204b7e240ceaeb7310f",
+    "url": "/rock-paper-scissors/static/js/main.f204b7e2.chunk.js"
+  },
+  {
+    "revision": "243d3df4419fae9ebbc1",
+    "url": "/rock-paper-scissors/static/js/1.243d3df4.chunk.js"
+  },
+  {
+    "revision": "f204b7e240ceaeb7310f",
+    "url": "/rock-paper-scissors/static/css/main.412f26d1.chunk.css"
+  },
+  {
+    "revision": "cb24687dd4d411019da87da965ff5d5a",
+    "url": "/rock-paper-scissors/index.html"
+  }
+];
